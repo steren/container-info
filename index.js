61eb26a3cb6b4db27c74b2916b97fef971bfe321
Steren Giannini <steren@google.com>
2019-03-19 23:18:18 -0700

    Report CPUs, memory and user

--- index.js
+++ index.js
@@ -2,23 +2,40 @@ const express = require('express');
 const app = express();
 
 const fs = require('fs');
+const os = require("os");
+const process = require('process');
 
 app.get('/', (req, res) => {
-  console.log('Hello world received a request.');
+  console.log('Received a request.');
 
   const files = [
       "/sys/fs/cgroup/memory/memory.usage_in_bytes",
       "/sys/fs/cgroup/memory/memory.limit_in_bytes",
       "/sys/fs/cgroup/cpu/cpuacct.usage",
       "/sys/fs/cgroup/cpu/cpu.shares",
+      "/sys/fs/cgroup/cpu/cpu.cfs_quota_us",
       ]
 
-  let results = {};
-  for (let f of files) {
-    results[f] = fs.readFileSync(f, 'utf8');
-  } 
+  let cgroup = {};
+  try {
+    for (let f of files) {
+        cgroup[f] = fs.readFileSync(f, 'utf8');
+    }
+  } catch(e) {
+      console.error("cannot read cgroup info");
+  }
 
-  res.send(results);
+  let memory = {
+      freemem: os.freemem(),
+      totalmem: os.totalmem(),
+      memoryUsage: process.memoryUsage(),
+  };
+
+  let cpus = os.cpus();
+
+  let username = os.userInfo().username
+
+  res.send({cgroup, memory, cpus, username});
 });
 
 const port = process.env.PORT || 8080;
